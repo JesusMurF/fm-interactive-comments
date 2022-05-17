18cdbec963e4570ca1f0c8f8c429e9aa375f59ab
Jesús Mur <murfontanals@gmail.com>
2022-05-17 22:26:42 +0200

    feat: :sparkles: vertical line for replies

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -15,76 +15,79 @@ import {
   StyledIncrementButton,
   StyledNumberedButton,
   StyledDecrementButton,
+  StyledVerticalLine,
 } from './style'
 
-export const Comment = ({ isReply = false, user = false }) => {
+export const Comment = ({ isReply = true, user = false }) => {
   return (
-    <StyledComment $isreply={isReply}>
-      {isReply && <hr className="float-left h-[110%] border-l-2" />}
-      <div className="hidden items-center justify-center md:flex md:w-1/6">
-        <StyledLikeButtons $vertical={true}>
-          <StyledIncrementButton $vertical={true}>+</StyledIncrementButton>
-          <StyledNumberedButton $vertical={true}>2</StyledNumberedButton>
-          <StyledDecrementButton $vertical={true}>-</StyledDecrementButton>
-        </StyledLikeButtons>
-      </div>
-      <div className="w-full md:w-5/6">
-        <StyledCommentHeader>
-          <StyledImageContainer>
-            <StyledImage src="./images/avatars/image-amyrobson.png" />
-          </StyledImageContainer>
-          <StyledUsername>Amy Robson</StyledUsername>
-          {user && <StyledLoggedUserBadge>you</StyledLoggedUserBadge>}
-          <StyledTime>one month ago</StyledTime>
-        </StyledCommentHeader>
-
-        <StyledCommentText>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
-          provident, nostrum doloremque similique natus recusandae, esse modi
-          tempore quia dolore, odit numquam omnis reprehenderit necessitatibus
-          animi ex hic ipsum! Excepturi?
-        </StyledCommentText>
-        <StyledCommentFooter>
-          <StyledLikeButtons>
-            <StyledIncrementButton>+</StyledIncrementButton>
-            <StyledNumberedButton>2</StyledNumberedButton>
-            <StyledDecrementButton>-</StyledDecrementButton>
+    <div className="flex">
+      <StyledVerticalLine $reply={isReply} />
+      <StyledComment>
+        <div className="hidden items-center justify-center md:flex md:w-1/6">
+          <StyledLikeButtons $vertical={true}>
+            <StyledIncrementButton $vertical={true}>+</StyledIncrementButton>
+            <StyledNumberedButton $vertical={true}>2</StyledNumberedButton>
+            <StyledDecrementButton $vertical={true}>-</StyledDecrementButton>
           </StyledLikeButtons>
-          {user ? (
-            <Fragment>
-              <StyledDeleteButton role="button">
-                <img
-                  className="h-4"
-                  src="./icons/icon-delete.svg"
-                  alt="Delete comment"
-                />
-                <p className="px-2">Delete</p>
-              </StyledDeleteButton>
+        </div>
+        <div className="w-full md:w-5/6">
+          <StyledCommentHeader>
+            <StyledImageContainer>
+              <StyledImage src="./images/avatars/image-amyrobson.png" />
+            </StyledImageContainer>
+            <StyledUsername>Amy Robson</StyledUsername>
+            {user && <StyledLoggedUserBadge>you</StyledLoggedUserBadge>}
+            <StyledTime>one month ago</StyledTime>
+          </StyledCommentHeader>
+
+          <StyledCommentText>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
+            provident, nostrum doloremque similique natus recusandae, esse modi
+            tempore quia dolore, odit numquam omnis reprehenderit necessitatibus
+            animi ex hic ipsum! Excepturi?
+          </StyledCommentText>
+          <StyledCommentFooter>
+            <StyledLikeButtons>
+              <StyledIncrementButton>+</StyledIncrementButton>
+              <StyledNumberedButton>2</StyledNumberedButton>
+              <StyledDecrementButton>-</StyledDecrementButton>
+            </StyledLikeButtons>
+            {user ? (
+              <Fragment>
+                <StyledDeleteButton role="button">
+                  <img
+                    className="h-4"
+                    src="./icons/icon-delete.svg"
+                    alt="Delete comment"
+                  />
+                  <p className="px-2">Delete</p>
+                </StyledDeleteButton>
+                <StyledEditButton
+                  role="button"
+                  onClick={() => console.log('click')}>
+                  <img
+                    className="h-4"
+                    src="./icons/icon-edit.svg"
+                    alt="Edit comment"
+                  />
+                  <p className="px-2">Edit</p>
+                </StyledEditButton>
+              </Fragment>
+            ) : (
               <StyledEditButton
                 role="button"
                 onClick={() => console.log('click')}>
                 <img
                   className="h-4"
-                  src="./icons/icon-edit.svg"
-                  alt="Edit comment"
+                  src="./icons/icon-reply.svg"
+                  alt="Reply comment"
                 />
-                <p className="px-2">Edit</p>
+                <p className="px-2">Reply</p>
               </StyledEditButton>
-            </Fragment>
-          ) : (
-            <StyledEditButton
-              role="button"
-              onClick={() => console.log('click')}>
-              <img
-                className="h-4"
-                src="./icons/icon-reply.svg"
-                alt="Reply comment"
-              />
-              <p className="px-2">Reply</p>
-            </StyledEditButton>
-          )}
-        </StyledCommentFooter>
-      </div>
-    </StyledComment>
+            )}
+          </StyledCommentFooter>
+        </div>
+      </StyledComment>
+    </div>
   )
 }

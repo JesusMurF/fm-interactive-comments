d3887cea4b0a8efa77e664886945f88b018fe840
Jesús Mur Fontanals <murfontanals@gmail.com>
2022-05-17 18:13:29 +0200

    style: :art: apply responsive design for comment reply

--- src/components/ReplyComment/ReplyComment.jsx
+++ src/components/ReplyComment/ReplyComment.jsx
@@ -2,21 +2,25 @@ import React from 'react'
 import {
   StyledCommentInput,
   StyledReplyContainer,
-  StyledReplyFooter,
+  StyledReplyImageContainer,
+  StyledReplyButtonContainer,
 } from './style'
 
 export default function ReplyComment() {
   return (
     <StyledReplyContainer>
       <StyledCommentInput placeholder="Add a comment..."></StyledCommentInput>
-      <StyledReplyFooter>
+      <StyledReplyImageContainer>
         <img
-          className="mt-2 mb-4 h-8 w-8"
+          className="mt-2 mb-4 h-8 w-8 md:h-12 md:w-12"
           src="./images/avatars/image-juliusomo.png"></img>
+      </StyledReplyImageContainer>
+
+      <StyledReplyButtonContainer>
         <button className="text-md mt-2 mb-4 h-10 w-20 rounded-lg bg-[#5358b6] text-xs font-semibold text-white">
           SEND
         </button>
-      </StyledReplyFooter>
+      </StyledReplyButtonContainer>
     </StyledReplyContainer>
   )
 }

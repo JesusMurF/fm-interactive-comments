f0e26520d76d3f7fa9008068ec0e2ac8ac213145
Jesús Mur <murfontanals@gmail.com>
2022-06-01 10:46:47 +0200

    refactor: :building_construction: Apply react composition and delete Comments container component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import CommentsContainer from './components/CommentsContainer/CommentsContainer'
+import React, { useState } from 'react'
+
+import { StyledCommentsContainer } from './style'
+import { Comment } from './components/Comment/Comment'
+import ReplyTextBox from './components/ReplyTextBox/ReplyTextBox'
+
+import data from '../data.json'
 
 function App() {
+  const [{ comments }] = useState(data)
+
   return (
-    <div>
-      <CommentsContainer></CommentsContainer>
-    </div>
+    <StyledCommentsContainer>
+      {comments.map((comment) => {
+        return <Comment key={comment.id} comment={comment}></Comment>
+      })}
+      <ReplyTextBox></ReplyTextBox>
+    </StyledCommentsContainer>
   )
 }
 
